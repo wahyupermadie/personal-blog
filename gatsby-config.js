a480ffdfda6b0b978e163a0a60eb4386c5b0b0c1
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2019-11-14 12:22:14 +0700

    add: new post about dagger

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   siteMetadata: {
-    title: 'Ghost',
-    description: 'The professional publishing platform',
+    title: 'Wahyu Permadi',
+    description: 'WePe Personal',
     siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
   },
   mapping: {
